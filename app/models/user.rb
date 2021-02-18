68f610ee8b4bd75c38fbb3cbd5582b3428bcbbf1
takashi3802 <shikataudone@yahoo.co.jp>
2021-02-18 22:24:14 +0900

    新規登録時入力バリデーション実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,10 +4,20 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :nickname       , presence: true
-  validates :first_name     , presence: true
-  validates :last_name      , presence: true
-  validates :first_name_kana, presence: true 
-  validates :last_name_kana , presence: true
-  validates :birth_day      , presence: true
+  validates :nickname,        presence: true
+  validates :birth_day,       presence: true
+
+  with_options presence: true, format: { with: /\A[ァ-ヶ]+\z/, message: 'Full-width katakana characters' } do
+    validates :first_name_kana
+    validates :last_name_kana
+  end
+
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/, message: 'Full-width characters' } do
+    validates :first_name
+    validates :last_name
+  end
+
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+    validates_format_of :password, with: PASSWORD_REGEX, message: 'Include both letters and numbers' 
 end
+
