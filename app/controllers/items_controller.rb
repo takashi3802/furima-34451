28c6dc98ebb3f5c34edb7939ae5cebaa3a2cbd27
takashi3802 <shikataudone@yahoo.co.jp>
2021-02-23 18:48:31 +0900

    ログアウト状態の時URLで編集ページに遷移しようとするとログインページに遷移

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new]
+  before_action :move_to_index, except: [:index, :show ]
 
   def index
     @items = Item.includes(:user).order('created_at DESC')
@@ -43,6 +44,12 @@ class ItemsController < ApplicationController
   def item_params
     params.require(:item).permit(:image, :name, :descriptions, :category_id, :status_id, :delivery_fee_id, :delivery_area_id,
                                  :delivery_day_id, :price).merge(user_id: current_user.id)
+  end
+
+  def move_to_index
+    unless user_signed_in? 
+      redirect_to action: :index
+    end
   end
 
 end
