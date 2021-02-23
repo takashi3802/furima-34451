b002bb0eea09306d988e02481ba9b7c77ae81c26
takashi3802 <shikataudone@yahoo.co.jp>
2021-02-23 19:47:54 +0900

    edit,updateアクション修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :edit]
-  before_action :move_to_index, except: [:index, :show]
+  before_action :move_to_index, except: [:edit, :update]
   before_action :set_item, only:[:show, :edit, :update]
 
   def index
@@ -47,7 +47,7 @@ class ItemsController < ApplicationController
   end
 
   def move_to_index
-    redirect_to action: :index unless user_signed_in?
+    redirect_to action: :index unless current_user.id == @item.user.id
   end
 
   def set_item
