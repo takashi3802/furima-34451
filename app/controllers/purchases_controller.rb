2f4ea1aa7fb75eaceb2b8f199a1f59251df8af86
takashi3802 <shikataudone@yahoo.co.jp>
2021-02-25 11:27:53 +0900

    購入情報保存

---
+++ app/controllers/purchases_controller.rb
@@ -0,0 +1,22 @@
+class PurchasesController < ApplicationController
+
+  def index
+    @purchase_address = PurchaseAddress.new
+  end
+
+  def create
+    @purchase_address = PurchaseAddress.new(purchase_params)
+    if @purchase_address.valid?
+      @purchase_address.save
+      redirect_to root_path
+    else
+      render :index
+    end
+  end
+
+  private
+
+  def  purchase_params
+   params.require(:purchase_address).permit(:postal_code, :delivery_area_id, :municipalities, :house_number, :building, :phone).merge(user_id: current_user.id)
+  end
+end
---
+++ app/models/address.rb
@@ -0,0 +1,3 @@
+class Address < ApplicationRecord
+  belongs_to :belongs_to :purchase
+end
