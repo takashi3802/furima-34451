7205494a9d09cd3f8e3287d274ccc21dafe3703f
takashi3802 <shikataudone@yahoo.co.jp>
2021-02-25 22:04:36 +0900

    購入情報保存

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,13 +1,12 @@
 class PurchasesController < ApplicationController
 
   def index
-    @purchase_address = PurchaseAddress.new
+   @purchase_address = PurchaseAddress.new(purchase_params)
   end
 
   def create
     @purchase_address = PurchaseAddress.new(purchase_params)
-    if @purchase_address.valid?
-      @purchase_address.save
+    if @purchase_address.save
       redirect_to root_path
     else
       render :index
@@ -17,6 +16,6 @@ class PurchasesController < ApplicationController
   private
 
   def  purchase_params
-   params.require(:purchase_address).permit(:postal_code, :delivery_area_id, :municipalities, :house_number, :building, :phone).merge(user_id: current_user.id)
+   params.permit(:postal_code, :delivery_area_id, :municipalities, :house_number, :building, :phone, :item_id).merge(user_id: current_user.id)
   end
 end
--- app/models/address.rb
+++ app/models/address.rb
@@ -1,3 +1,3 @@
 class Address < ApplicationRecord
-  belongs_to :belongs_to :purchase
+  belongs_to :purchase
 end
