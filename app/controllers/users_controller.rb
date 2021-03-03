e6da8fa784cf9a920d00cfe25f0f830e6bdec149
takashi3802 <shikataudone@yahoo.co.jp>
2021-03-03 19:45:36 +0900

    ユーザー編集、削除

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,14 @@
 class UsersController < ApplicationController
-
+  before_action :authenticate_user!, only: [:show, :edit,:update,:edit, :destroy]
+  before_action :set_user, only: [:update ,:edit, :destroy]
+  before_action :move_to_index, only: [:show, :update ,:edit, :destroy]
+  
   def show
     @user = User.find(params[:id])
   end
 
   def update
-  end
-
-  def edit
-    if current_user.save(user_params)
+    if current_user.update(user_params)
       sign_in(current_user, bypass:true)
       redirect_to root_path
     else
@@ -16,8 +16,10 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit
+  end
+
   def destroy
-     user = User.find(params[:id])
      user.destroy
      redirect_to root_path
   end
@@ -28,4 +30,14 @@ class UsersController < ApplicationController
     params.permit(:nickname, :email, :password, :last_name, :first_name, :last_name_kana, :first_name_kana, :birth_day) 
   end
 
+  def set_user
+    user = User.find(params[:id])
+  end
+
+  def move_to_index
+    user = User.find(params[:id])
+    redirect_to root_path unless current_user.id == user.id
+  end
+
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   get 'users/show'
   devise_for :users
   root to: "items#index"
-  resources :users, only: [:show, :edit, :destroy]
+  resources :users, only: [:show, :edit, :destroy, :update]
   resources :items do
     resources :purchases, only: [:index, :create]
   end
